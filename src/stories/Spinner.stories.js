c574bd651fce902c5ba75cd585cd77ba003404eb
pawanpyakurel30 <pawan.pyakurel@satyatech.xyz>
2020-06-03 08:22:20 +0545

    storybook setup and sucessfully tested in spinner components

---
+++ src/stories/Spinner.stories.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Spinner } from "../components/Spinner";
+
+export const SpinnerLoder = () =>(
+    <Spinner />
+)
+
+export default {
+    title: 'Spinner',
+    component: Spinner,
+  };
+
+  
