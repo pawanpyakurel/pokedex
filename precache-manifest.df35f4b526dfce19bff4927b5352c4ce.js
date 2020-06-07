179d1457650a3e53f92d336a01b81c95fd2fcb41
pawanpyakurel30 <pawan.pyakurel@satyatech.xyz>
2020-06-07 19:27:19 +0545

    Updates

---
+++ precache-manifest.df35f4b526dfce19bff4927b5352c4ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d80c5da753311e61e83363e3b5627b76",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "cd73c61100a733f6aec9",
+    "url": "/pokedex/static/css/2.55a347cb.chunk.css"
+  },
+  {
+    "revision": "cd73c61100a733f6aec9",
+    "url": "/pokedex/static/js/2.fbbc1cb6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/pokedex/static/js/2.fbbc1cb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0ef1e83ae3957148bdb",
+    "url": "/pokedex/static/js/main.06817091.chunk.js"
+  },
+  {
+    "revision": "3720d53d7f3565cf8d9c",
+    "url": "/pokedex/static/js/runtime-main.115cfb4e.js"
+  },
+  {
+    "revision": "8d66d96a9893ee18763d913767db869f",
+    "url": "/pokedex/static/media/favicon.8d66d96a.png"
+  }
+]);
