21517c10fbac472a4b0b3398463fb44173a58748
pawanpyakurel30 <pawan.pyakurel@satyatech.xyz>
2020-06-07 19:29:45 +0545

    hosting in github.io

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,22 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
+//new added
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store, persistor } from "./config/store";
+import { PersistGate } from "redux-persist/lib/integration/react";
+
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
+  const { getByText } = render(
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
+  );
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
